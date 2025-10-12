3badb462d6ce911f2a909633c1c4b2ec85bc7c2e
Aya <ayaliz@waifu.club>
2025-10-13 00:13:57 +0200

    Setup

--- src/pages/SetupGuidePage.tsx
+++ src/pages/SetupGuidePage.tsx
@@ -6,11 +6,11 @@ const SetupGuidePage = () => {
         <div>
             
             <h4>Step 1: Compile Hachimi-Unity2020</h4>
-            <p>The plugin API is more recent than the latest release of Hachimi-Unity2020. You will need to compile the current version yourself. If you need help, please consult a friend or your favorite LLM. I can not provide compiled binaries for someone else's project.</p>
+            <p>Data capture is done via a plugin for hachimi, a mod for the game. The plugin API is more recent than the latest release of Hachimi-Unity2020, so you will need to compile the current version yourself. If you need help, consult a friend or your favorite LLM. I can not provide compiled binaries for someone else's project.</p>
             <p>Once compiled, rename the DLL to <strong>winhttp.dll</strong> and place it in the root of your game folder.</p>
 
 			<h4>Step 2: Download horseACT.dll</h4>
-			<p>Download <a href="data/horseACT.dll" download><strong>horseACT.dll</strong></a> and place it in the root of your game folder. This is a hachimi plugin to capture race data.</p>
+			<p>Download <a href="data/horseACT.dll" download><strong>horseACT.dll</strong></a> and place it in the root of your game folder. This is the hachimi plugin to capture race data.</p>
 			<p style={{ marginTop: '4px', lineHeight: 0.1 }}> <small style={{ color: 'gray' }}>This will mean nothing to most of you, but this hooks the same functions as CarrotBlender so the two can not be used together.</small></p>  
 			
             <h4>Step 3: Configure Hachimi</h4>
@@ -27,7 +27,7 @@ const SetupGuidePage = () => {
             </pre>
 
             <h4>Step 4: To the races!</h4>
-            <p>Restart your game if it's currently running. Your career and room matches (including CM) will now be saved in the <strong>Saved races</strong> folder inside of your Documents folder.</p>
+            <p>Restart your game if it's currently running. Your career and room matches (including CM) will now be saved in the <strong>Saved races</strong> folder inside of your <strong>Documents</strong> folder.</p>
             <p>If you'd like a different save location, you can specify a save path in <strong>horseACTConfig.json</strong> inside the <strong>hachimi</strong> folder.</p>
 
             <h4>Step 5: Parse race data</h4>
